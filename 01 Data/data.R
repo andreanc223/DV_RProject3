42933d6e571bd44a29830eb072afc959492143c1
Andrea Curo <andreanicole@utexas.edu>
2015-02-26 16:10:58 -0600

    added library calls

--- 01 Data/data.R
+++ 01 Data/data.R
@@ -1,5 +1,10 @@
+library(RCurl)
+library(dplyr)
+library(tidyr)
+
 dfa <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from tsleepalert"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_thc359', PASS='orcl_thc359', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 dfb <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from tbackground"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_thc359', PASS='orcl_thc359', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
+dfc <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from tactivity"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_thc359', PASS='orcl_thc359', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 
-dfc <- inner_join(dfa, dfb, by="ID_NUMBER")
-dfd <- left_join(dfa, dfb, by="ID_NUMBER")
+dfd <- inner_join(dfa, dfb, by="ID_NUMBER")
+dfe <- left_join(dfa, dfb, by="ID_NUMBER")
