3244a220885f7497ce9aba63ba15e713c7635248
sonamshah <sonam.shah17@yahoo.com>
2015-03-02 19:03:36 -0600

    fixed sleep_joblength

--- 02 Data Wrangling/df_sleep_joblength.R
+++ 02 Data Wrangling/df_sleep_joblength.R
@@ -3,8 +3,6 @@ library(dplyr)
 library(tidyr)
 library(jsonlite)
 library(scales)
-library(ggplot2)
-library(ggthemes)
 
 dfa <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from tsleepalert"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_thc359', PASS='orcl_thc359', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
@@ -13,4 +11,5 @@ dfb <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?qu
 dfc <- right_join(dfa, dfb, by="ID_NUMBER")
 
 
-dfc %>% select(YEARS_IN_PRESENT_JOB, SLEEP_LENGTH, SLEEP_QUALITY) %>% group_by(SLEEP_QUALITY) %>% ggplot(aes(as.numeric(as.character(SLEEP_LENGTH)), as.numeric(as.character(YEARS_IN_PRESENT_JOB)),  color = SLEEP_QUALITY))+geom_point() + ggtitle('Years Worked in Comparison to Sleep Length and Quality') +theme(plot.title=element_text(size=20,face="bold",vjust=1,lineheight=0.6)) + labs(x = 'Sleep Length (hours)', y = 'Years Worked') + geom_jitter(alpha=0.5,aes(color=SLEEP_QUALITY),position=position_jitter(width=.2)) + scale_colour_tableau
+dfc %>% select(YEARS_IN_PRESENT_JOB, SLEEP_LENGTH, SLEEP_QUALITY) %>% group_by(SLEEP_QUALITY) %>% ggplot(aes(as.numeric(as.character(SLEEP_LENGTH)), as.numeric(as.character(YEARS_IN_PRESENT_JOB)),  color = SLEEP_QUALITY))+geom_point() + ggtitle('Years Worked in Comparison to Sleep Length and Quality') +theme(plot.title=element_text(size=20,face="bold",vjust=1,lineheight=0.6)) + labs(x = 'Sleep Length (Years)', y = 'Years Worked') + geom_jitter(alpha=0.5,aes(color=SLEEP_QUALITY),position=position_jitter(width=.2)) + scale_colour_tableau()
+
