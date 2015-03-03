8a5dd8bc55f23fb9771a4387aceffd38c7501e44
Andrea Curo <andreanicole@utexas.edu>
2015-03-03 15:49:26 -0600

    attempted png

--- 01 Data/data.R
+++ 01 Data/data.R
@@ -2,6 +2,8 @@ library(RCurl)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(png)
+library(grid)
 
 dfa <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from tsleepalert"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_thc359', PASS='orcl_thc359', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
@@ -12,5 +14,28 @@ dfc <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?qu
 head(dfa)
 head(dfb)
 head(dfc)
+
+categoricals <- eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from tsleepalert"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_thc359', PASS='orcl_thc359', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'True'), verbose = TRUE), 1, 2^31-1)))
+ddf <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from tsleepalert"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_thc359',PASS='orcl_thc359',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)));
+
+names(ddf)
+categoricals[[1]]
+l <- list()
+for (i in names(ddf)) { 
+  if (i %in% categoricals[[1]]) {
+    r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\", count(*) n from tsleepalert group by \\\""i"\\\" "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL',USER='C##cs329e_thc359',PASS='orcl_thc359',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
+    p <- myplot(r,i)
+    print(p) 
+    l[[i]] <- p
+  }
+}
+
+png("C:/cs329e/DataVisualization/DV_RProject3/00 Doc/categoricals.png", width = 25, height = 10, units = "in", res = 72)
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(1, 12)))   
+
+print(l[[1]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:4))
+print(l[[2]], vp = viewport(layout.pos.row = 1, layout.pos.col = 5:8))
+print(l[[3]], vp = viewport(layout.pos.row = 1, layout.pos.col = 9:12))
 
 
