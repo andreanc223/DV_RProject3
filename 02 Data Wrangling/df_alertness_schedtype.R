14da74f38c5f419f441a471d08b42c336a254a01
unknown <theodorahcheng@gmail.com>
2015-03-03 17:38:10 -0600

    hello

--- 02 Data Wrangling/df_alertness_schedtype.R
+++ 02 Data Wrangling/df_alertness_schedtype.R
@@ -2,6 +2,8 @@ library(RCurl)
 library(dplyr)
 library(tidyr)
 library(jsonlite)
+library(ggplot2)
+require(jsonlite)
 
 dfa <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from tsleepalert"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_thc359', PASS='orcl_thc359', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
 
