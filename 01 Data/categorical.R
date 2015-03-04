7998b901fdaa2e12289237a5e7bd981c9bf45154
unknown <theodorahcheng@gmail.com>
2015-03-04 00:26:32 -0600

    who

--- 01 Data/categorical.R
+++ 01 Data/categorical.R
@@ -8,6 +8,7 @@ library(jsonlite)
 require("tidyr")
 require("dplyr")
 require("jsonlite")
+require("png")
 
 myplot <- function(df, x) {
   names(df) <- c("x", "n")
@@ -29,12 +30,42 @@ for (i in names(ddf)) {
   }
 }
 
-png("C:/cs329e/DataVisualization/DV_RProject3/00 Doc/categoricals.png", width = 25, height = 10, units = "in", res = 72)
+png("C:\\Users\\dora\\Documents\\DV_RProject3\\00 Doc\\categoricals.png", width = 25, height = 10, units = "in", res = 72)
 grid.newpage()
 pushViewport(viewport(layout = grid.layout(1, 12)))   
 
 print(l[[1]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:4))
 print(l[[2]], vp = viewport(layout.pos.row = 1, layout.pos.col = 5:8))
 print(l[[3]], vp = viewport(layout.pos.row = 1, layout.pos.col = 9:12))
+
+dev.off()
+
+myplot1 <- function(df, x) {
+  names(ddf) <- c("x")
+  ggplot(ddf, aes(x=x)) + geom_histogram()
+}
+l <- list()
+for (i in names(ddf)) {   
+  # For details on [[...]] below, see http://stackoverflow.com/questions/1169456/in-r-what-is-the-difference-between-the-and-notations-for-accessing-the
+  if (i %in% categoricals[[2]]) {
+    r <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select \\\""i"\\\" from tsleepalert where \\\""i"\\\" is not null "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal',USER='C##cs329e_thc359',PASS='orcl_thc359',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON', i=i),verbose = TRUE)))
+    p <- myplot1(r,i)
+    print(p) 
+    l[[i]] <- p
+  }
+}
+
+png("C:\\Users\\dora\\Documents\\DV_RProject3\\00 Doc\\categoricals2.png", width = 25, height = 20, units = "in", res = 72)
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(2, 12)))   
+
+print(l[[1]], vp = viewport(layout.pos.row = 1, layout.pos.col = 1:3))
+print(l[[2]], vp = viewport(layout.pos.row = 1, layout.pos.col = 4:6))
+print(l[[3]], vp = viewport(layout.pos.row = 1, layout.pos.col = 7:9))
+print(l[[4]], vp = viewport(layout.pos.row = 1, layout.pos.col = 10:12))
+print(l[[5]], vp = viewport(layout.pos.row = 2, layout.pos.col = 1:3))
+print(l[[6]], vp = viewport(layout.pos.row = 2, layout.pos.col = 4:6))
+print(l[[7]], vp = viewport(layout.pos.row = 2, layout.pos.col = 7:9))
+print(l[[8]], vp = viewport(layout.pos.row = 2, layout.pos.col = 10:12))
 
 dev.off()
